4e2aa3dc5efe9e4d6a62ed51b207aa0e35f7dae8
Nimira43 <n.rabone72@aol.com>
2026-08-29 23:23:11 +0100

    Setting up React client.

---
+++ client/vite.config.ts
@@ -0,0 +1,15 @@
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
+
+export default defineConfig({
+  server: {
+    port: 3000
+  },
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    mkcert()
+  ],
+})
