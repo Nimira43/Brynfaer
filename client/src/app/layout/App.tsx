5b38466f776c5711d634c7c362c9db45867390d1
Nimira43 <n.rabone72@aol.com>
2026-09-09 05:00:52 +0100

    Configured light/dark mode switch.

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,12 +1,54 @@
 import { useEffect, useState } from 'react'
 import type { Product } from '../models/product'
 import Catalog from '../../features/catalog/Catalog'
-import { Box, Container, CssBaseline, ThemeProvider } from '@mui/material'
+import { Box, Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import NavBar from './NavBar'
-import theme, { darkMode } from '../../theme'
 
 function App() { 
   const [products, setProducts] = useState<Product[]>([])
+  const [darkMode, setDarkMode] = useState(false)
+  const palleteType = darkMode ? 'dark' : 'light'
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#00bfa6',
+        dark: '#008c78',
+        light: '#5fffe8',
+        contrastText: '#fff'
+      },
+      secondary: {
+        main: '#ff6f61',
+        dark: '#cc4f45',
+        light: '#ff9a8f',
+        contrastText: '#fff'
+      },
+      mode: palleteType,
+      background: {
+        default: (palleteType === 'light')
+          ? '#eaeaea'
+          : '#111111'
+      }
+    },
+    typography: {
+      fontFamily: 'Poppins, sans-serif'
+    },
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            textTransform: 'none',
+            fontWeight: 400,
+            borderRadius: 2
+          }
+        }
+      }
+    }
+  })
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
 
   useEffect(() => {
     fetch('https://localhost:5001/api/products')
@@ -17,19 +59,22 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <NavBar />
+      <NavBar
+        toggleDarkMode={toggleDarkMode}
+        darkMode={darkMode}    
+      />
       <Box
         sx={{
           minHeight: '100vh',
           background: darkMode
-            ? '#111111'
-            : '#eaeaea'
-        }}
-      
+            ? 'radial-gradient(circle, #005f51,#00483d)'
+            : 'radial-gradient(circle, #c8fff9,#e7fffc)',
+          py: 6
+        }}    
       >
         <Container
           maxWidth='xl'
-          sx={{marginTop: 14}}
+          sx={{marginTop: 8}}
           >  
           <Catalog products={products}/>     
         </Container>   
--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,6 +1,15 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { DarkMode, LightMode } from '@mui/icons-material'
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
 
-export default function NavBar() {
+type Props = {
+  toggleDarkMode: () => void
+  darkMode : boolean
+}
+
+export default function NavBar({
+  darkMode,
+  toggleDarkMode
+}: Props) {
   return (
     <AppBar position='fixed'>
       <Toolbar>
@@ -9,6 +18,9 @@ export default function NavBar() {
             Brynfaer
           </span>
         </Typography>
+        <IconButton onClick={toggleDarkMode}> 
+          {darkMode ? <DarkMode /> : <LightMode />}
+        </IconButton>
       </Toolbar>
     </AppBar>
   )
