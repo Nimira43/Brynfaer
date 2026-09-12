13bd4aa23d1e6f6ffd9f324b3ca6d5302988f335
Nimira43 <n.rabone72@aol.com>
2026-09-12 02:19:27 +0100

    Setting up React Router.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/layout/styles.css'
-import App from './app/layout/App'
 import '@fontsource/poppins/100.css'
 import '@fontsource/poppins/200.css'
 import '@fontsource/poppins/300.css'
@@ -11,9 +10,11 @@ import '@fontsource/poppins/600.css'
 import '@fontsource/poppins/700.css'
 import '@fontsource/poppins/800.css'
 import '@fontsource/poppins/900.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/routes/Routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider  router={router} />
   </StrictMode>,
 )
