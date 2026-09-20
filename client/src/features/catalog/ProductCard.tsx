6cc618966aa7a9f47cfe8552aa29df3a5c24dc8d
Nimira43 <n.rabone72@aol.com>
2026-09-20 09:07:55 +0100

    ProductDetails page

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, CardMedia, Typography, CardActions, Button } from '@mui/material'
 import type { Product } from '../../app/models/product'
+import { Link } from 'react-router-dom'
 
 type Props = {
   product: Product
@@ -46,7 +47,10 @@ export default function ProductCard({product}: Props) {
         <Button>
           Add to Cart
         </Button>
-        <Button>
+        <Button
+          component={Link}
+          to={`/catalog/${product.id}`}
+        >
           View
         </Button>
       </CardActions>
